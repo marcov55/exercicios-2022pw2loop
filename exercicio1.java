08f47ed8c63ba23109f96e6f95097060f249f30a
ETEC <e164ti@cps.sp.gov.br>
2022-03-23 14:54:51 -0300

    exercicios

--- exercicio1.java
+++ exercicio1.java
@@ -1,16 +1,22 @@
 public class exercicio1 {
     public static void main(String[] args) {
-        // Arzena a soma
-        int soma = 0;
-        // Percorre os números de 1 à 500
-        for(int num = 1; num <= 500;num++) {
-            // Verificar se o número é ímpar
-            if ((num % 2) != 0) {
-                // Verifica se o número é multiplo de 3
-                if ((num % 3) != 0) {
-                     System.out.println(soma);
+       //Armazena a soma
+       int soma = 0;
+       // Percorre os números de 1 à 500
+       for(int num = 1;num <= 500;num++) {
+           // Verificar se o número é ímpar
+           if((num % 2) != 0) {
+               // Verifica se o número é multiplico de 3
+               if((num % 3) == 0) {
+                   //System.out.println(num);
+                   //soma += num;
+                   soma = soma + num;
+                }
+            }
         }
-    }
-}    
+        System.out.println(soma);
     }
 }
+
+
+            
